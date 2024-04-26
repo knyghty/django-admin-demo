b01dd40d35d519d604008a7d583cd635e4ea0e81
Tom Carrick <tom@carrick.eu>
2024-04-26 11:23:13 +0200

    Always use language prefix

--- demo/urls.py
+++ demo/urls.py
@@ -6,5 +6,4 @@ from django.urls import path
 urlpatterns = i18n_patterns(
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
-    prefix_default_language=False,
 )
