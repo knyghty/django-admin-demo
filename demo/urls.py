478c24758579a9636c6938916f07efcd21471986
Tom Carrick <tom@carrick.eu>
2024-06-02 13:58:30 +0200

    Redirect to admin from homepage

--- demo/urls.py
+++ demo/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from . import views
+
 urlpatterns = i18n_patterns(
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
+    path("", views.redirect_to_admin),
 )
---
+++ demo/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import redirect
+
+def redirect_to_admin(request):
+    return redirect("admin:index")
