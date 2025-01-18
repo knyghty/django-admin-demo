8c7550b8208c38937801e532169f0d3eab7c5750
Tom Carrick <tom@carrick.eu>
2025-01-18 12:44:20 +0100

    Add congrats page (#29)

--- demo/urls.py
+++ demo/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.debug import default_urlconf
 
 from . import views
 
 urlpatterns = i18n_patterns(
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
+    path("congrats/", default_urlconf, name="congrats"),
     path("", views.redirect_to_admin),
 )
